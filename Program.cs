1dcfc9ae1f895f71519cea63dc73143764cb7c4d
Expired <imoses2@hotmail.com>
2022-11-05 12:39:30 -0700

    basic outline, opens notepad

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Hello_Notepad {
 	
 	class Program {
 	
 		static void Main(string[] args) {
+			
+			// Check whether notepad is already open
+			
+			
+			// if it's not, open it
+			Process.Start("Notepad");
+			
+			
+			// click on File
+			
+			
+			// click on New
+			
+			
+			// these weren't mentioned in the challenge, but were mentioned by Ali Bakhta
+			// type 'hello' in the window
+			
+			
+			// save to desktop
+			
 			
 		}
 	}
