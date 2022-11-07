27c8525fa38ff57ac15254decabe8eece6b046ef
Expired <imoses2@hotmail.com>
2022-11-07 15:48:26 -0700

    uses keyboard shortcuts instead of mouse clicks

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace Hello_Notepad {
 	class Program {
 	
 		// FLAGS
-		static int LEFTDOWN = 0x00000002;
-		static int LEFTUP =   0x00000004;
 		static int SW_SHOWNORMAL = 1;
+		static uint KEYEVENTF_KEYUP = 0x0002;
+		static byte VK_CONTROL = 0x11;
+		static byte VK_N = 0x4E;
 		
 		// MAIN PROGRAM
 		static void Main(string[] args) {
@@ -41,62 +42,23 @@ namespace Hello_Notepad {
 			// wait
 			p.WaitForInputIdle();
 			
-			// get the window's location
-			Console.WriteLine("Get the location of the window...");
-			Rect location = new Rect();
-			GetWindowRect(p.MainWindowHandle, ref location);
-			// Console.WriteLine(location.Left);
-			// Console.WriteLine(location.Top);
-
-			// set the mouse position
-			Console.WriteLine("Set the mouse over the File Menu Item...");
-			SetCursorPos(location.Left + 25, location.Top + 40);
+			// Click on File -> New, using the keyboard to type Ctrl+N
+			Console.WriteLine("Type Ctrl+N to open a new file.");
+			keybd_event(VK_CONTROL, 0x45, 0, 0);
+			keybd_event(VK_N, 0x45, 0, 0);
+			keybd_event(VK_CONTROL, 0x45, KEYEVENTF_KEYUP, 0);
 			
-			// click on File
-			Console.WriteLine("And click File.");
-			mouse_event(LEFTDOWN, 0, 0, 0, 0);
-			mouse_event(LEFTUP, 0, 0, 0, 0);
-			
-			// click on New
-			Console.WriteLine("Set the mouse over the New Menu Item...");
-			SetCursorPos(location.Left, location.Top + 60);
-			Console.WriteLine("And click New.");
-			mouse_event(LEFTDOWN, 0, 0, 0, 0);
-			mouse_event(LEFTUP, 0, 0, 0, 0);
-			
-			
-			// these weren't mentioned in the challenge, but were mentioned by Ali Bakhta
-			// type 'hello' in the window
-			
-			
-			// save to desktop
-			
-			
-		}
-		
-		// HELPFUL OBJECTS
-		public struct Rect {
-			public int Left { get; set; }
-			public int Top { get; set; }
-			public int Right { get; set; }
-			public int Bottom { get; set; }
 		}
 		
 		// DLL IMPORTS
-		[DllImport("user32.dll")]
-		public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
-
-		[System.Runtime.InteropServices.DllImport("user32.dll")]
-		static extern bool SetCursorPos(int x, int y);
-
-		[System.Runtime.InteropServices.DllImport("user32.dll")]
-		public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-		
 		[DllImport("user32.dll")]
 		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 		
 		[DllImport("User32.dll")]
 		public static extern Int32 SetForegroundWindow(int hWnd);
+		
+		[DllImport("user32.dll")]
+		internal static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);	
 		
 	}
 }
